299b9fc660b15f1934c28f3f5646bfa994c96fdf
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-07-11 13:41:52 +0600

    sidebar added and topbar updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import Topbar from "./components/Topbar/Topbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Topbar/>
-    </div>
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        {/* <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newproduct">
+            <NewProduct />
+          </Route>
+        </Switch> */}
+      </div>
+    </Router>
   );
 }
 
